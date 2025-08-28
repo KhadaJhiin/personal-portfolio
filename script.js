fcc09a17fa1eee6cd3d91a49fb4173bb05a8db70
Jose <josemiguelortegagaviria@gmail.com>
2025-08-28 11:51:42 -0500

    header mobile's alredy done

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 const menuBtn = document.querySelector(".header__menu-btn");
 const nav = document.querySelector(".header__nav");
+const langButton = document.querySelector(".header__lang");
+const lang = document.querySelector(".lang__selected");
 
 menuBtn.addEventListener("click", ()=>{
     nav.classList.toggle("header__nav--open");
+});
+
+langButton.addEventListener("click", ()=>{
+    if(lang.textContent === "en"){
+        lang.textContent = "es";
+    }else{
+        lang.textContent = "en";
+    };
 });
 
