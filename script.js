9db30ba027b62dbd5f4389fde14d51de64dc270f
Jose <josemiguelortegagaviria@gmail.com>
2025-09-09 18:45:08 -0500

    Entire page modification

--- script.js
+++ script.js
@@ -6,19 +6,14 @@ const nav = document.querySelector(".header__nav");
 const overlay = document.querySelector(".overlay");
 const langButtons = document.querySelectorAll(".header__lang, .lang__selector");
 const lang = document.querySelectorAll(".lang__selected");
+const logo = document.querySelector(".header__logo");
+
 
 const swiper = new Swiper(".mySwiper", {
     effect: "coverflow",
     grabCursor: true,
     centeredSlides:true,
     slidesPerView: "auto",
-    coverflowEffect:{
-        rotate:15,
-        strech:0,
-        depth:300,
-        modifier:1,
-        slideShadows:true,
-    },
     loop:false,
     speed: 1000,
 
@@ -28,10 +23,31 @@ const swiper = new Swiper(".mySwiper", {
     }
 });
 
+const sections = document.querySelectorAll(".section");
+
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add("visible");
+        } else {
+            entry.target.classList.remove("visible"); 
+        }
+    });
+}, {
+    threshold: 0.2 // 20% visible para activar animación
+});
+
+sections.forEach(section => observer.observe(section));
+
+
+
 menuBtn.addEventListener("click", ()=>{
     nav.classList.toggle("header__nav--open");
     overlay.classList.toggle("active");
     document.body.classList.toggle("noscroll");
+    logo.classList.toggle("hide");
+    header.classList.toggle("overlayh");
+
 });
 
 langButtons.forEach(element =>{
@@ -50,21 +66,13 @@ langButtons.forEach(element =>{
 window.addEventListener("scroll", () => {
     const currentScroll = window.pageYOffset;
 
-    if (window.innerWidth <= 1024) {
-        if (currentScroll > lastScrollMobile && currentScroll > 50) {
-            menuBtn.classList.add("hide");
-        } else {
-            menuBtn.classList.remove("hide");
-        }
-        lastScrollMobile = currentScroll;
+    if (currentScroll > lastScrollMobile && currentScroll > 60) {
+        header.classList.add("hide");
     } else {
-        if (currentScroll > lastScrollDesktop && currentScroll > 50) {
-            header.classList.add("hide");
-        } else {
-            header.classList.remove("hide");
-        }
-        lastScrollDesktop = currentScroll;
+        header.classList.remove("hide");
     }
+    lastScrollMobile = currentScroll;
+
 });
 
 
